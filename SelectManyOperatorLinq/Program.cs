42a824b8c7419a1c3a3d48bd7b5046cb541a0fa7
ciorca <ciorca.gaby@gmail.com>
2022-06-25 14:04:39 +0300

    Add project files.

---
+++ SelectManyOperatorLinq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SelectManyOperatorLinq
+{
+    class Program
+    {
+       	     static void Main()
+
+        {
+            var result =  from student in Student.GetAllStudents()
+                          from subject in student.Subjects
+                          select new { StudentName = student.Name, SubjectName = subject};
+
+            foreach (var v in result)
+            {
+                Console.WriteLine(v.StudentName + " -" + v.SubjectName);
+            }
+
+
+        }
+    }
+    }
+
---
+++ SelectManyOperatorLinq/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectManyOperatorLinq
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Gneder { get; set; }
+        public List<string> Subjects { get; set; }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> listStudents = new List<Student>
+            {
+                new Student
+                {
+                    Name = "Tom",
+                    Gneder = "Male",
+                    Subjects = new List<string> { "ASP.NET", "C#" }
+                },
+                new Student
+                {
+                    Name = "Mike",
+                    Gneder = "Male",
+                    Subjects = new List<string> { "ADO.NET", "C#", "AJAX" }
+                },
+                new Student
+                {
+                    Name = "Pam",
+                    Gneder = "Female",
+                    Subjects = new List<string> { "WCF", "SQL Server", "C#" }
+                },
+                new Student
+                {
+                    Name = "Mary",
+                    Gneder = "Female",
+                    Subjects = new List<string> { "WPF", "LINQ", "ASP.NET" }
+                },
+
+            };
+            return listStudents;
+        }
+    }
+}
+
+    
+
